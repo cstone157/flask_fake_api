7d10f611221cce9d0044f9e0d8e529cf98ad6cf9
cstone157 <culver.stone57@gmail.com>
2024-08-09 09:55:55 -0700

    Update flask-api.py

--- app/flask-api.py
+++ app/flask-api.py
@@ -1,3 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/greet/<string:name>', methods=['GET'])
+def greet(name):
+    return jsonify(message=f"Hello, {name}!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
+'''
 # flask-app.py
 from flask import Flask, request
 import json
@@ -54,3 +67,4 @@ def square():
   tmp = _messages
   _messages = []
   return json.dumps(tmp)
+'''
